b179c9914827d407918f4fe179990f168f6a5b3e
ttran <tuan43@gmail.com>
2019-02-17 12:39:32 -0600

    add auth and logging

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const secure = require('express-force-https');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bodyParser = require("body-parser");
@@ -7,24 +6,31 @@ const bcrypt = require('bcrypt');
 const helmet = require('helmet');
 const fs = require('fs');
 const https = require('https');
-const http = require('http');
+const _ = require('lodash');
 
 const app = express();
-const authUser = {
+const users = [{
   username: 'tuan43',
   hashedPassword: '$2b$10$0kdaeo/a9T.cXEvppsl8oO6S0b3a2fuHgZL.7zb3EdxCrGS3OVrLq',
   authKey: '1529 6939'
-};
+}, {
+  username: 'sbui',
+  hashedPassword: '$2b$10$90SPNdBml3GHcJJG7C9rA.5HGUJS5otRDucInmnDYDVpfPPSNFRu.',
+  authKey: 'I love my mochi! :)'
+}];
 
 app.use(helmet());
-app.use(secure);
 app.use(express.static('build'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+let authUser = null;
+
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    if (username !== authUser.username) {
+    authUser = _.find(users, { username: username });
+
+    if (!authUser) {
       return done(null, false, { message: 'Incorrect username/password.'});
     }
 
@@ -41,6 +47,7 @@ passport.use(new LocalStrategy(
 app.post('/login', 
   passport.authenticate('local', { session: false, failureRedirect: '/' }), 
   function(req, res) {
+    console.log('authUser authenticated ' + authUser);
     res.json({ authKey: authUser.authKey });
   });
 
@@ -48,11 +55,6 @@ const httpsOptions = {
   key: fs.readFileSync('./certs/key.pem'),
   cert: fs.readFileSync('./certs/cert.pem')
 };
-
-http.createServer(function (req, res) {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
 
 const server = https.createServer(httpsOptions, app).listen(443, () => {
   console.log('Watch Tower running at ' + 443)
