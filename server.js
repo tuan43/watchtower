d86b9fec3f8119f9852ba1ba78bb9e056d3a1be2
ttran <tuan43@gmail.com>
2019-02-16 23:44:08 -0600

    tabs to spaces

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const port = 3000;
 
 const authUser = {
-	username: 'tuan43',
-	hashedPassword: 'X'
+  username: 'tuan43',
+  hashedPassword: '$2b$10$0kdaeo/a9T.cXEvppsl8oO6S0b3a2fuHgZL.7zb3EdxCrGS3OVrLq'
 };
 
 app.use(secure);
@@ -20,24 +20,24 @@ app.use(bodyParser.json());
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-  	if (username !== authUser.username) {
-  		return done(null, false, { message: 'Incorrect username/password.'});
-  	}
+    if (username !== authUser.username) {
+      return done(null, false, { message: 'Incorrect username/password.'});
+    }
 
     bcrypt.compare(password, authUser.hashedPassword, function(err, res) {
-		  if (res) {
-		    return done(null, { message: 'yay' });
-		  } else {
-		    return done(null, false, { message: 'Incorrect username/password.'});
-		  } 
-		});
+      if (res) {
+        return done(null, { message: 'yay' });
+      } else {
+        return done(null, false, { message: 'Incorrect username/password.'});
+      } 
+    });
   }
 ));
 
 app.post('/login', 
-	passport.authenticate('local', { session: false, failureRedirect: '/' }), 
-	function(req, res) {
-    res.json({ key: 'tobi123' });
+  passport.authenticate('local', { session: false, failureRedirect: '/' }), 
+  function(req, res) {
+    res.json({ authKey: '1529 6939' });
   });
 
 app.listen(port, () => console.log(`Watchtower listening on port ${port}!`));
