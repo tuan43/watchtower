d7c15a2f2417530d6105bf0f42eff94f28cc5185
ttran <tuan43@gmail.com>
2019-03-02 11:29:17 -0600

    add custom mui styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import './index.css';
 import App from './components/App';
 
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+        main: '#4252B3'
+      }
+    }
+});
+
+
 ReactDOM.render(
 	<BrowserRouter>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
 	</BrowserRouter>, document.getElementById('root'));
